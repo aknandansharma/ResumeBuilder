588259723652ad333e2a4c2d5eb589e88c635da0
Aknandan Sharma <90132050+aknandansharma@users.noreply.github.com>
2022-11-18 21:08:17 +0530

    almost complite

--- script.js
+++ script.js
@@ -27,3 +27,54 @@ function addNewAEField() {
 }
 
 
+
+// Genrating CV
+
+function generateCV(){
+  console.log("Genrate CV");
+
+  let nameField = document.getElementById("nameField").value;
+  let nameT1 = document.getElementById("nameT1");
+  nameT1.innerHTML = nameField;
+
+  // Nmae
+  document.getElementById("nameT2").innerHTML = nameField;
+
+  // contact
+  document.getElementById("contactT").innerHTML =
+    document.getElementById("contactField").value;
+  // email
+  document.getElementById("emailT").innerHTML =
+    document.getElementById("emailField").value;
+  // address
+  document.getElementById("addressT").innerHTML =
+    document.getElementById("addressField").value;
+
+  // Links
+  //Profile linkl
+  document.getElementById("webT").innerHTML =
+    document.getElementById("webField").value;
+  // twitter
+  document.getElementById("twitterT").innerHTML =
+    document.getElementById("twitterField").value;
+  // git
+  document.getElementById("gitT").innerHTML =
+    document.getElementById("gitField").value;
+  // linkedin
+  document.getElementById("linkedT").innerHTML =
+    document.getElementById("linkedField").value;
+
+  // Objective
+  document.getElementById("objectiveT").innerHTML =
+    document.getElementById("objectiveField").value;
+
+  // Work exp
+  let wes = document.getElementById('weField')
+  let str = "";
+  for(let e of wes){
+    str = str + `<li> ${e.value} </li>`;
+  }
+  document.getElementById('weT').innerHTML = str;
+
+
+}
